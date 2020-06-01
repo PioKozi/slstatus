79e945002915e86491ec3da0a0b971fdc971e598
PioKozi <47647338+PioKozi@users.noreply.github.com>
2020-06-01 18:09:37 +0100

    changed formatting for date

--- config.h
+++ config.h
@@ -63,7 +63,7 @@ static const char unknown_str[] = "n/a";
  */
 static const struct arg args[] = {
 	/* function format          argument */
-	{ datetime, " %s | ",           "%F %T" },
+	{ datetime, " %s | ",           "%T | %a %d %b" },
     { battery_state, "%s ",      "BAT0" },
     { battery_perc, "%s%% ",        "BAT0" },
     { battery_remaining, "%s | ",   "BAT0" },
